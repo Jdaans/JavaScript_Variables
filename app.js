343ebd96c668c5f34d47df1fb9f89dc0723a4813
Josh Daniels <jdaans95@gmail.com>
2025-02-12 13:52:25 -0800

    JS variables practice

--- app.js
+++ app.js
@@ -3,8 +3,36 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
 
-// Exercise 2
+const firstName = "Josh";
+let lastName = "Daniels";
+var age = 30;
 
-// Exercise 3
+
+lastName = "Daniels";
+age = 30;
+
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(`${language} was first released in ${createdYear}.`);
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(`There are currently no size ${selectedSize} in stock.`);
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`The book ${title} was written by ${author} and is approximatley ${pageCount} pages long.`);
+
+
